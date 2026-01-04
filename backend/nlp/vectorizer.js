c6a059a22ce36de9c49aa7e42d58879775e85e40
Joy chowdhury <cjoy882@gmail.com>
2026-01-04 18:57:37 +0600

    Integrate preprocessing and add cosine similarity
    Updated vectorizer functions to use preprocessing and added cosine similarity function.

--- backend/nlp/vectorizer.js
+++ backend/nlp/vectorizer.js
@@ -1,4 +1,6 @@
+// vectorizer.js
 import natural from "natural";
+import { preprocess } from "./preprocess.js";
 
 const TfIdf = natural.TfIdf;
 const tfidf = new TfIdf();
@@ -9,18 +11,35 @@ const tfidf = new TfIdf();
 export function trainVectorizer(intents) {
   intents.forEach((intent) => {
     intent.examples.forEach((sentence) => {
-      tfidf.addDocument(sentence);
+      tfidf.addDocument(preprocess(sentence));
     });
   });
 }
 
 /**
- * Convert a sentence into a TF-IDF vector
+ * Convert sentence into TF-IDF vector
  */
 export function vectorize(sentence) {
   const vector = [];
-  tfidf.tfidfs(sentence, (i, measure) => {
+  tfidf.tfidfs(preprocess(sentence), (i, measure) => {
     vector.push(measure);
   });
   return vector;
+}
+
+/**
+ * Cosine similarity (NEW)
+ */
+export function cosineSimilarity(vecA, vecB) {
+  let dot = 0,
+    normA = 0,
+    normB = 0;
+
+  for (let i = 0; i < vecA.length; i++) {
+    dot += vecA[i] * vecB[i];
+    normA += vecA[i] ** 2;
+    normB += vecB[i] ** 2;
+  }
+
+  return dot / (Math.sqrt(normA) * Math.sqrt(normB) || 1);
 }
